fb5e5f0f09da0735a31d6d7ee68fa8da9011a559
Kit <kitsimcoe@gmail.com>
2023-04-23 23:38:48 +0100

    Added basic grid layout with flexbox

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
-let numberOfSquares = document.querySelector('input').value;
-console.log(numberOfSquares);
+let numberOfSquares = 16
+console.log(numberOfSquares)
+
+document.querySelector('#submit').onclick = function() {
+  let numberOfSquares = document.querySelector('#numberOfSquares').value;
+  console.log(numberOfSquares);
+}
+
+
+
+
+function addSquares
+for (i = 1; i <= numberOfSquares; i++) {
+    const container = document.querySelector('#container');
+    let newColumn = document.createElement('div');
+    newColumn.classList.add('column');
+    container.appendChild(newColumn);
+}
+
+
+
+for (i = 1; i <= numberOfSquares; i++) {
+const columns = document.querySelectorAll('.column');
+for (let column of columns) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    column.appendChild(square)
+}
+}
+
+
+  
+
+// for (const column in columns) {
+//     columns.appendChild(square)
+// }
+
+
+
+
+
+
+
